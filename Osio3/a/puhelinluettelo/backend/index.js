a65d7f227ba411689300241e81d05784087db7b2
Topi Purtola <topi.purtola@gmail.com>
2024-06-13 15:50:11 +0300

    En tiiä gitti ihan solmussa

--- Osio3/a/puhelinluettelo/backend/index.js
+++ Osio3/a/puhelinluettelo/backend/index.js
@@ -4,6 +4,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const yhtTiedot = require('./models/yhtTiedot')
 
 app.use(cors())
 
@@ -20,90 +22,107 @@ app.use(express.json());
 
 
 
-const kovacode = 
-  [
-    {
-      "name": "Lauri Penttinen",
-      "number": "345678987654",
-      "id": 1
-    },
-    {
-      "name": "Pentti Laurinen",
-      "number": "09876546789",
-      "id": 2
-    },
-    {
-      "name": "Jorma",
-      "number": "456789",
-      "id": 3
-    }
-  ]
+const Lista = [ ]
 
   app.get('/', (request, response) => {
   response.send("pekka")    
 })
 
-app.post('/api/persons', (request, response) => {
-  const { name, number } = request.body;
 
-  if (!name || !number) {
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  
+  if (!body.name || !body.number) {
     return response.status(400).json({ error: "Name or number not found" });
   }
 
-  if (kovacode.some(entry => entry.name === name)) {
+  if (Lista.some(entry => entry.name === body.name)) {
     return response.status(409).json({ error: "Name must be unique" });
   }
 
+  
+/*
   const id = Math.floor(Math.random() * 100000);
-  const currentDate = new Date(); // Get the current date
+  const currentDate = new Date(); 
+*/
+  const yhteystieto = new yhtTiedot({
+  name: body.name,
+  number: body.number,
+  })
 
-  kovacode.push({ name, number, id });
-  console.log(kovacode); // Print the updated phonebook to the console
+  yhteystieto.save().then(savedtieto => {
+    response.json(savedtieto)
+  })
 
+
+/*
   return response.status(201).json({
+    name: yhteystieto.name,
+    number: yhteystieto.number,
     message: "Entry added successfully",
     id,
-    date: currentDate.toISOString() // Convert current date to ISO string
+    date: currentDate.toISOString() 
   });
+  */
+ 
 });
 
 
 app.get ('/info', (request,response) => {
-  response.send(
-    "Phonebook has the info of "+kovacode.length+" people<br>"+
-currentdate)
+  const currentDate = new Date(); 
+
+    yhtTiedot.countDocuments({})
+    .then(count => {
+      response.send ("Phonebook has the info of "+ count +" people<br>"+currentDate)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-   response.send(kovacode)
+  yhtTiedot.find({}).then(Lista =>{
+    response.json(Lista)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  console.log(id)
-  const lista = kovacode.find(lista => lista.id == id)
-
-  if (lista) {
-    response.send(lista)
-  }
-  else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  yhtTiedot.findById(request.params.id)
+    .then(yhteystieto => {
+      if (yhteystieto) {
+        response.json(yhteystieto)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id); 
-  const index = kovacode.findIndex(person => person.id === id);
 
-  if (index != -1) {
-    kovacode.splice(index, 1); 
-    response.status(204).end(); 
-  } else {
-    response.status(404).end(); 
+app.delete('/api/persons/:id', (request, response, next) => {
+  yhtTiedot.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
-});
 
+  next(error)
+}
 
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
